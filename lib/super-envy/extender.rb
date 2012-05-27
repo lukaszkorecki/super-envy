d09edc63379252d76e4cfe24571252f9bda514ad
Łukasz Korecki <lukasz@coffeesounds.com>
2012-05-27 22:14:14 +0100

    more derp

--- lib/super-envy/extender.rb
+++ lib/super-envy/extender.rb
@@ -14,15 +14,12 @@ module SuperEnvy
     end
 
     def create_accessors!
-      @hash.keys.map {|k| [k.to_s.gsub('-', '_').to_sym , k ] }.each do |d,e|
+      @hash.keys.map {|k| [k.to_s.gsub('-', '_').to_sym , k ] }.each do |name,original_key|
 
-        puts d.inspect
-        puts e.inspect
-        name, original_key = d
         puts "definiingin method: #{name}"
         puts "with keyj #{original_key}"
         puts "and value #{@hash[original_key]}"
-        define_method name { @hash[original_key] }
+        self.define_method(name) { @hash[original_key] }
       end
     end
   end
